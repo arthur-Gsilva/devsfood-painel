828bd896e2a9ce8890fcb1968739a36cfd607818
arthur-Gsilva <122728865+arthur-Gsilva@users.noreply.github.com>
2023-08-18 12:03:10 -0300

    modal done

--- src/app/(inside)/produtos/page.tsx
+++ src/app/(inside)/produtos/page.tsx
@@ -95,7 +95,9 @@ export default function Produtos () {
             />
 
             {modal &&
-                <ProductModal />
+                <ProductModal 
+                    setClose={setModal}
+                />
             }
             
         </div>
--- src/components/ProductModal/index.tsx
+++ src/components/ProductModal/index.tsx
@@ -1,19 +1,49 @@
 import styles from './styles.module.css'
 
-export const ProductModal = () => {
+
+type Props = {
+    setClose: (a: boolean) => void
+}
+
+export const ProductModal = ({ setClose }: Props) => {
     return(
         <div className={styles.container}>
             <div className={styles.modalContent}>
 
                 <h2>Edite o Produto</h2>
-                <form>
 
-                    <label htmlFor="image">Adicione uma imagem</label>
-                    <input type='name' id='image'/>
+                <form className={styles.form}>
 
-                    <label htmlFor="number">Adicione um número</label>
-                    <input type="number" id="number" />
+                    <div>
+                        <label htmlFor="image">Imagem</label>
+                        <input type='file' id='image' accept='image/*' placeholder='escolher imagem'/>
+                    </div>
+
+                    <div>
+                        <label htmlFor="name">Nome</label>
+                        <input type="text" id="name" />
+                    </div>
+
+                    <div>
+                        <label htmlFor="price">Preço</label>
+                        <input type="number" id='price'/>
+                    </div>
+
+                    <div>
+                        <label htmlFor="description">Descrição</label>
+                        <textarea name="description" id="description" cols={30} rows={10} className={styles.textArea}></textarea>
+                    </div>
                 </form>
+
+                <div className={styles.actions}>
+                    <button 
+                        style={{backgroundColor: 'red'}}
+                        onClick={() => setClose(false)}
+                    >
+                        Cancelar
+                    </button>
+                    <button>Salvar</button>
+                </div>
             </div>
         </div>
     )
